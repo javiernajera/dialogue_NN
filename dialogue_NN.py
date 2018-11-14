4bd32c29d171ad0ca0b751099bf4b5d869c45a7d
javiernajera <34201757+javiernajera@users.noreply.github.com>
2018-11-14 10:54:48 -0500

    Add files via upload

---
+++ dialogue_NN.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import os
+import json
+import datetime
+import csv
+import numpy as np
+import time
+
+def get_raw_training_data(fileName):
+    
+
+def main():
+  stemmer = LancasterStemmer()
+  raw_training_data = get_raw_training_data('dialogue_data.csv')
+
+
+if __name__ == "__main__":
+    main()
