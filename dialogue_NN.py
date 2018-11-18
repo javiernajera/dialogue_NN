9388026500537eca01427754d723ca9acbdc9d62
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-18 16:25:11 -0500

    setting up organize function

--- dialogue_NN.py
+++ dialogue_NN.py
@@ -8,6 +8,8 @@ import numpy as np
 import time
 import re
 
+
+
 def preprocess_words(words, stemmer):
     stem_list = []
     for word in words:
@@ -25,16 +27,28 @@ def get_raw_training_data(fileName):
         for row in dialogue:
             text = ' '.join(row)
             match = pattern.match(text)
-            raw_training_data.append({"person" : match.group(1), "sentence" : match.group(2)})
+            raw_training_data.append({"person": match.group(1), "sentence": match.group(2)})
 
             #raw_training_data.append({"person" : text_arr[0], "sentence" : text_arr[1]})
     return raw_training_data
 
+def organize_raw_training_data(training_data, stemmer):
+    words = []
+    for element in training_data:
+        print(element)
+        print(element['sentence'])
+
+        words.append(element['sentence'])
+
+
+
+
+
 def main():
-  stemmer = LancasterStemmer()
-  raw_training_data = get_raw_training_data('dialogue_data.csv')
-  print(stemmer.stem("swimming"))
-  #print(raw_training_data)
+    # nltk.download('punkt')
+    stemmer = LancasterStemmer()
+    raw_training_data = get_raw_training_data('dialogue_data.csv')
+    organize_raw_training_data(raw_training_data, stemmer)
 
 if __name__ == "__main__":
     main()
